4b37b381d4585853b8634a2514e88766d9e2bb3a
Darshan Ponikar <drsofficial3k@gmail.com>
2024-03-08 00:16:06 +0530

    fix: layout

--- src/components/layout/hstack.tsx
+++ src/components/layout/hstack.tsx
@@ -2,7 +2,7 @@ import React, { FC, PropsWithChildren } from "react";
 
 export const HStack: FC<PropsWithChildren> = ({ children }) => {
   return (
-    <section className="w-full flex gap-4 flex-row items-center">
+    <section className="w-full flex gap-4 flex-row items-start">
       {children}
     </section>
   );
