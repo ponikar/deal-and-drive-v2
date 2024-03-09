74004d06600a6e4d64771c8669986413aae00af7
Darshan Ponikar <drsofficial3k@gmail.com>
2024-03-09 22:40:45 +0530

    feat: coming soon page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,43 @@
-import { InputForm } from "@/components/forms/sample";
+import { Metadata } from "next";
+import Image from "next/image";
+import Link from "next/link";
 
-export default function Home() {
+import { cn } from "@/lib/utils";
+import { buttonVariants } from "@/components/ui/button";
+import { LoginForm } from "@/components/forms/login";
+
+export const metadata: Metadata = {
+  title: "Deal and Drive | Coming Soon",
+  description: "",
+};
+
+export default function AuthenticationPage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <InputForm />
-    </main>
+    <>
+      <div className="container relative h-full flex-col items-center justify-center grid lg:max-w-none lg:px-0">
+        <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+          <div className="relative z-20 mx-auto flex items-center text-lg font-medium">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="mr-2 h-6 w-6"
+            >
+              <path d="M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3" />
+            </svg>
+            Deal and Drive Inc
+          </div>
+          <div className="flex flex-col space-y-2 text-center">
+            <h1 className="text-2xl font-semibold tracking-tight">
+              Coming Soon
+            </h1>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
